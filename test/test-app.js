463b6b4df8f2997b2d27035d23b2c9c26b10ef53
unknown <brennan5@XE0806000822.global.avaya.com>
2015-02-02 13:06:36 +0000

    Add fs-extra as dependency

--- test/test-app.js
+++ test/test-app.js
@@ -1,27 +1,31 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs-extra');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
-var os = require('os');
 
 describe('ngbp-module:app', function () {
-  console.log(os.tmpdir(), './temp-test');
+
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
+      .inDir(path.join(__dirname, './temp'))
       .withOptions({ 'skip-install': true })
-      .withArguments(['testModule'])
+      .withArguments(['newModule'])
       .withPrompt({
         rootFolder: 'app',
         modules: ['templates', 'uiRouter']
+      })
+      .on('ready', function () {
+        fs.writeFileSync('./src/app/app.js', "angular.module('test', ['existingModule'])");
       })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'src/app/testModule/testModule.module.js'
+      'src/app/app.js',
+      'src/app/newModule/newModule.module.js'
     ]);
   });
 });
