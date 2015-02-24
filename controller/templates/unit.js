ca21b6bb42b0e99351bd656fc1f12c894b724412
John Brennan <jbrennan@psychwire.org>
2015-02-24 23:40:31 +0000

    Adding scaffolded unit test for controller sub-generator

---
+++ controller/templates/unit.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: <%= filename %>', function () {
+
+  // load the controller's module
+  beforeEach(module('<%= namespace %>'));
+
+  var <%= filename %>,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    <%= filename %> = $controller('<%= filename %>', {
+      $scope: scope
+    });
+  }));
+
+  it('should expect scope to be defined', function () {
+    expect(scope).toBeDefined();
+  });
+});
