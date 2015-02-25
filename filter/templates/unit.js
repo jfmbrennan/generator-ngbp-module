199e0f95328e397d30bc2c9d4a97ae6bc5a66105
John Brennan <jbrennan@psychwire.org>
2015-02-25 21:05:48 +0000

    Adding template for filter unit test

---
+++ filter/templates/unit.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Filter: <%= filename %>', function () {
+
+  // load the filter's module
+  beforeEach(module('<%= namespace %>'));
+
+  var <%= filename %>,
+    scope;
+
+  // Initialize the filter and a mock scope
+  beforeEach(inject(function ($filter, $rootScope) {
+    scope = $rootScope.$new();
+    <%= filename %> = $filter('<%= filename %>', {
+      $scope: scope
+    });
+  }));
+
+  it('should expect scope to be defined', function () {
+    expect(scope).toBeDefined();
+  });
+});
