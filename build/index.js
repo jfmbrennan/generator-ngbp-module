f39dbb29ea03ce5402799a68996a7d38eee1c1ac
John Brennan <jfmbrennan@gmail.com>
2015-03-11 22:43:44 +0000

    Added logic and tempaltes to scaffold app if one does not exist

---
+++ build/index.js
@@ -0,0 +1,55 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+var Generator = module.exports = function Generator(args, options) {
+  yeoman.generators.Base.apply(this, arguments);
+  this.argument('appname', {type: String, required: false});
+  this.appname = this.appname || path.basename(process.cwd());
+  this.initApp = options.init;
+};
+
+util.inherits(Generator, yeoman.generators.Base);
+
+Generator.prototype.askForModules = function askForModules() {
+  if (!this.initApp) {
+    var done = this.async();
+
+    var prompts = {
+      type: "confirm",
+      name: "createProject",
+      message: "Cannot find package.json. Do you want to scaffold a new app?"
+    };
+
+    this.prompt(prompts, function (props) {
+      this.initApp = props.createProject;
+      done();
+    }.bind(this));
+  }
+};
+
+
+Generator.prototype.writeAppFiles = function writeAppFiles() {
+  if (this.initApp) {
+
+    var files = this.expandFiles('**', { dot: true, cwd: this.templatePath() });
+
+    for (var i = 0; i < files.length; i++) {
+      var f = files[i];
+      var src = path.join(this.templatePath(), f);
+      if (path.basename(f).indexOf('_') == 0) {
+          var dest = path.join(path.dirname(f), path.basename(f).replace(/^_/, ''));
+          this.template(src, dest);
+      } else {
+          this.copy(src, f);
+      }
+    }
+  }
+};
+
+Generator.prototype.updateConfig = function updateConfig() {
+  if (this.initApp) {
+    this.config.set('name', this.appname);
+  }
+};
